386db3748c48dce4fd44f8de0819c4d0e7b5a6c1
markel149 <markel.orallo@gmail.com>
2020-05-10 03:01:06 +0200

    Calculadora finished

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -1,5 +1,5 @@
 #Suma, resta, multiplicacion, division, raiz cuadrada y exponentes
-
+from math import sqrt
 def menuPrincipal():
 
     print("Calculadora:\n")
@@ -40,21 +40,67 @@ def menuPrincipal():
         except ValueError:
             print("Elige una opcion entre 0 y 6")
     exit
-# falta el try y catch
+
+def primernumero():
+    global n1
+    try:
+        n1 = int(input("Introduce un numero:"))
+    except ValueError:
+        print("El caracter introducido es invalido. Introduce un numero.")
+        primernumero()
+    return n1
+
+def segundonumero():
+    global n2
+    try:
+        n2 = int(input("Introduce el segundo numero:"))
+    except ValueError:
+        print("El caracter introducido es invalido. Introduce un numero.")
+        segundonumero()
+    return n2
+
 def suma():
-    a = int(input("Introduce el primer numero:"))
-    b = int(input("Introduce el segundo numero:"))
-    print("Resultado:",a+b)
+    print("Resultado:",primernumero()+segundonumero())
     input("Pulsa cualquier tecla para volver al menu.")
     print("--------------------------------------")
     menuPrincipal()
 
-def resta():
-    print("resta")
+def resta():    
+    print("Resultado:",primernumero()-segundonumero())
+    input("Pulsa cualquier tecla para volver al menu.")
+    print("--------------------------------------")
+    menuPrincipal()   
+
+def multi():
+    print("Resultado:",primernumero()*segundonumero())
+    input("Pulsa cualquier tecla para volver al menu.")
+    print("--------------------------------------")
     menuPrincipal()
 
+def divi():
     
+    a = primernumero()
+    b = segundonumero()
+    if b==0:
+        print("No se puede dividir entre 0.")
+    else:
+        print("Resultado:",a/b)
+    input("Pulsa cualquier tecla para volver al menu.")
+    print("--------------------------------------")
+    menuPrincipal()
 
+def expo():
+    
+    print("Resultado:",primernumero()**segundonumero())
+    input("Pulsa cualquier tecla para volver al menu.")
+    print("--------------------------------------")
+    menuPrincipal()
+
+def raiz():    
+    print("Resultado:",sqrt(primernumero))
+    input("Pulsa cualquier tecla para volver al menu.")
+    print("--------------------------------------")
+    menuPrincipal()
 
 
 menuPrincipal()
